d04266d7d39898b17de36d62059ddf65bc8f5959
alsdk1676 <alsdk6761@gmail.com>
2025-05-18 23:42:39 +0900

    global-style

--- src/styles/global-style.js
+++ src/styles/global-style.js
@@ -8,6 +8,18 @@ const GlobalStyle = createGlobalStyle`
   @font-face {
       font-family: 'Pretendard-Regular';
       src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+      font-weight: 300;
+      font-style: light;
+  }
+  @font-face {
+      font-family: 'Pretendard-Regular';
+      src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+      font-weight: 400;
+      font-style: medium;
+  }
+  @font-face {
+      font-family: 'Pretendard-Regular';
+      src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
       font-weight: 400;
       font-style: normal;
   }
